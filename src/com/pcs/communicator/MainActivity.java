3f47375fcf7a950c7706211941ed112484650457
krsola <krsola@CE04778.corp.capgemini.com>
2013-06-15 15:05:22 +0200

    day schedule

--- src/com/pcs/communicator/MainActivity.java
+++ src/com/pcs/communicator/MainActivity.java
@@ -1,19 +1,37 @@
 package com.pcs.communicator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	private Button button;
+
+	private class ButtonOnClickListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this,
+					DayScheduleActivity.class);
+			startActivity(intent);
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_view);
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new ButtonOnClickListener());
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_view, menu);
 		return true;
 	}
